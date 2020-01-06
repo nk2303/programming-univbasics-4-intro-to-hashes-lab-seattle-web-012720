027e625fd206b9105dc0d40eead99ce3d423321f
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-06 06:57:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,6 +15,10 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => 'Grace Hopper'
+  }
+  return pioneer
 end
 
 def id_generator
